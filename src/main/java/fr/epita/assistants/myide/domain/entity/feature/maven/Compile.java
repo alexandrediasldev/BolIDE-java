c27f52ddc41ef94f6b2a1a97878579fc44db50f8
Martin Chevereau <martin.chevereau@epita.fr>
2021-06-09 22:39:44 +0200

    maven compile done

--- src/main/java/fr/epita/assistants/myide/domain/entity/feature/maven/Compile.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/feature/maven/Compile.java
@@ -4,10 +4,49 @@ import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
 
+import java.io.*;
+
 public class Compile implements Feature {
+    private InputStream stream;
+
     @Override
     public ExecutionReport execute(Project project, Object... params) {
-        return null;
+        class Report implements ExecutionReport
+        {
+            private boolean success;
+
+            public Report(boolean success) {
+                this.success = success;
+            }
+
+            @Override
+            public boolean isSuccess() {
+                return success;
+            }
+
+            public void setSuccess(boolean success) {
+                this.success = success;
+            }
+        }
+        Report report = new Report(false);
+
+        try {
+            var command = "mvn compile ";
+            for(var p : params) {
+                command += (String)p;
+            }
+
+            var exe = MavenExecuter.mvnCommand(command);
+            if(exe != 0) {
+                return report;
+            }
+        } catch (IOException | InterruptedException e) {
+            return report;
+        }
+
+
+        report.setSuccess(true);
+        return report;
     }
 
     @Override
---
+++ src/main/java/fr/epita/assistants/myide/domain/entity/feature/maven/StreamHandler.java
@@ -0,0 +1,31 @@
+package fr.epita.assistants.myide.domain.entity.feature.maven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+class StreamHandler extends Thread {
+
+    private final InputStream inputStream;
+
+    public StreamHandler(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    public void run(boolean isError) {
+        try {
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(reader);
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                if(isError)
+                    System.err.println("Err:"+line);
+                else
+                    System.out.println(line);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
