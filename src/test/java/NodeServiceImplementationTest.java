3a445bc92c0def2228b31df19f30f2d8f6e87b5b
Martin Chevereau <martin.chevereau@epita.fr>
2021-06-12 11:10:05 +0200

    fixed compile erro on test

--- src/test/java/NodeServiceImplementationTest.java
+++ src/test/java/NodeServiceImplementationTest.java
@@ -4,7 +4,6 @@ import fr.epita.assistants.myide.domain.entity.node.Folder;
 import fr.epita.assistants.myide.domain.service.NodeServiceImplementation;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.testng.Assert;
 
 import java.io.FileWriter;
 import java.io.IOException;
