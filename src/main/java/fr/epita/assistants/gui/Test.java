67903c2596d2fb5c5c3eb761d93b407755f5ca45
Anonymous <anonymous@example.com>
2021-06-15 12:36:34 +0200

    added abstract class for main pane + played with the graphic editor

---
+++ src/main/java/fr/epita/assistants/gui/Test.java
@@ -0,0 +1,12 @@
+package fr.epita.assistants.gui;
+
+import javax.swing.*;
+
+public class Test {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JTextArea WriteSomecode;
+    private JPanel OutputTerminal;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+}
