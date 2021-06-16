769ebb30508effa5dd9fe5020381dcbf9f8f1bdf
mathias.deroche <mathias.deroche@epita.fr>
2021-06-16 23:35:23 +0200

    fixed scrolling for text edition area

--- src/main/java/fr/epita/assistants/gui/TextEditor.java
+++ src/main/java/fr/epita/assistants/gui/TextEditor.java
@@ -6,24 +6,24 @@ import java.awt.*;
 public class TextEditor extends JPanel {
 
     public TextEditor() {
-        setSize(1000,1000);
+        //setSize(1000,1000);
         JTabbedPane Tabs = new JTabbedPane();
         JTextArea text = new JTextArea();
-        Font font = new Font("MS Gothic", Font.PLAIN, 24);
+        Font font = new Font("Comic Sans MS", Font.PLAIN, 24);
 
         text.setFont(font);
         text.setForeground(Color.white);
         text.setBackground(Color.darkGray);
 
+        JScrollPane scrollpane = new JScrollPane(text);
+        scrollpane.setPreferredSize(new Dimension(1300, 700));
 
 
-        text.setPreferredSize(new Dimension(1000, 1000));
         JPanel pan = new JPanel();
-        pan.setSize(1000,1000);
-        pan.add(text);
+        //pan.setSize(1000,600);
+        pan.add(scrollpane);
         Tabs.add(pan, "MAIN");
-        Tabs.setSize(1000,1000);
+        //Tabs.setPreferredSize(new Dimension(1000,600));
         add(Tabs);
-
     }
 }
