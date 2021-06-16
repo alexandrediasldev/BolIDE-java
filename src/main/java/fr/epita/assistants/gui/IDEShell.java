e1626ecbc6fa6698a7c7bd914c93f4f8bcce771c
Alexandre Castello <alexandre.castello@epita.fr>
2021-06-16 22:15:05 +0200

    fake shell implemented odes nothing but text

---
+++ src/main/java/fr/epita/assistants/gui/IDEShell.java
@@ -0,0 +1,17 @@
+package fr.epita.assistants.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class IDEShell extends JPanel {
+    public IDEShell()
+    {
+        JTextArea text = new JTextArea();
+        Font font = new Font("ARIAL", Font.PLAIN, 24);
+        text.setFont(font);
+        text.setPreferredSize(new Dimension(1000,200));
+        text.setForeground(Color.lightGray);
+        text.setBackground(Color.darkGray);
+        add(text);
+    }
+}
