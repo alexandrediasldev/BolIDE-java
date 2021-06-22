dc9f507c72517abef762b98e4ac2bc57b85ef44e
mathias.deroche <mathias.deroche@epita.fr>
2021-06-22 22:43:40 +0200

    Started to implement the file loading FEATURE

---
+++ src/main/java/fr/epita/assistants/gui/LoadFile.java
@@ -0,0 +1,61 @@
+package fr.epita.assistants.gui;
+
+import fr.epita.assistants.myide.domain.entity.Node;
+import fr.epita.assistants.myide.domain.entity.node.File;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class LoadFile implements TreeSelectionListener {
+    private File file;
+    private RSyntaxTextArea textArea;
+
+    public LoadFile(File file, RSyntaxTextArea textArea) {
+        this.file = file;
+        this.textArea = textArea;
+    }
+
+    public LoadFile(Node file) {
+        if (!(file instanceof File))
+            throw new IncompatibleClassChangeError("Node is not a File!");
+
+        this.file = (File) file;
+    }
+
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    private String loadNodeContent() {
+        String content = "";
+
+        try
+        {
+            content = new String(Files.readAllBytes(file.getPath()));
+        }
+        catch (IOException e)
+        {
+            System.err.println("Failed to load file");
+        }
+
+        return content;
+    }
+
+    public void loadText()
+    {
+        textArea.setText(loadNodeContent());
+    }
+
+    @Override
+    public void valueChanged(TreeSelectionEvent e) {
+
+    }
+}
