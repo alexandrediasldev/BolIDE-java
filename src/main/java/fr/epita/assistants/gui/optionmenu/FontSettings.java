cec355ca503f61b85f42bbb1ab592b8dd1f0de59
Martin Chevereau <martin.chevereau@epita.fr>
2021-06-22 22:41:07 +0200

    added font selection

---
+++ src/main/java/fr/epita/assistants/gui/optionmenu/FontSettings.java
@@ -0,0 +1,40 @@
+package fr.epita.assistants.gui.optionmenu;
+
+import fr.epita.assistants.gui.TextEditor;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FontSettings extends JFrame {
+    public FontSettings(TextEditor textEditor) {
+        super("Font settings");
+        var pannel = new JPanel();
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] fontsAvailable = ge.getAvailableFontFamilyNames();
+        JComboBox<String> fontBox = new JComboBox<String>(fontsAvailable);
+
+        fontBox.setEditable(true);
+        pannel.add(fontBox);
+        var selectButton = new JButton("SELECT");
+
+        selectButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (fontBox.getSelectedIndex() != -1) {
+                    var selected  = fontBox.getItemAt
+                            (fontBox.getSelectedIndex());
+                    var text = textEditor.getText();
+                    text.setFont(new Font(selected, Font.PLAIN, 24));
+                }
+                //set font with res
+            }
+        });
+        pannel.add(selectButton);
+
+
+        add(pannel);
+        pack();
+    }
+}
