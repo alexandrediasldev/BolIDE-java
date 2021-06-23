b1f33321162a45b1d2ecd4addc43398fb1072156
Anonymous <anonymous@example.com>
2021-06-23 22:51:48 +0200

    added implements keyword for treepanel, separated the listener function to only have to call this

--- src/main/java/fr/epita/assistants/gui/TreePanel.java
+++ src/main/java/fr/epita/assistants/gui/TreePanel.java
@@ -12,7 +12,7 @@ import java.awt.*;
 import java.io.File;
 import java.nio.file.Path;
 
-public class TreePanel extends JPanel {
+public class TreePanel extends JPanel  implements TreeSelectionListener{
 
 
    public TreePanel(Node root)
@@ -20,25 +20,25 @@ public class TreePanel extends JPanel {
 
        IDENodes parent = new IDENodes(root);
        JTree treeFile = new JTree(parent);
-       treeFile.addTreeSelectionListener(new TreeSelectionListener() {
-           public void valueChanged(TreeSelectionEvent e) {
-               IDENodes node = (IDENodes) e
-                       .getPath().getLastPathComponent();
-
-               if (node.getNode().isFile())
-               {
-                   var load = new LoadFile(node.getNode(), IDEFrame.getText());
-                   load.loadText();
-               }
-
-           }
-       });
+       treeFile.addTreeSelectionListener(this);
        JScrollPane scrollpane = new JScrollPane();
        scrollpane.getViewport().add(treeFile);
        // check more precisely later
        scrollpane.setPreferredSize(new Dimension(300, 1000));
        add(scrollpane);
    }
+
+    @Override
+    public void valueChanged(TreeSelectionEvent e) {
+        IDENodes node = (IDENodes) e
+                .getPath().getLastPathComponent();
+
+        if (node.getNode().isFile())
+        {
+            var load = new LoadFile(node.getNode(), IDEFrame.getText());
+            load.loadText();
+        }
+    }
 
 
 
