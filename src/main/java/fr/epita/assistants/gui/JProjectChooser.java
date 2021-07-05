4acab9463bcd24293f235cb1120edc82ff753767
alexandre.dias <alexandre.dias@epita.fr>
2021-07-05 16:54:21 +0200

    Added missing file from last commit

---
+++ src/main/java/fr/epita/assistants/gui/JProjectChooser.java
@@ -0,0 +1,29 @@
+package fr.epita.assistants.gui;
+
+import javax.swing.*;
+import java.io.File;
+
+public class JProjectChooser {
+    JFileChooser fileChooser = new JFileChooser();
+    JFrame mainPanel;
+    public JProjectChooser(JFrame mainPanel)
+    {
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        this.mainPanel = mainPanel;
+
+    }
+    public boolean choose()
+    {
+        int option = fileChooser.showOpenDialog(mainPanel);
+        if(option == JFileChooser.APPROVE_OPTION){
+
+            File file = fileChooser.getSelectedFile();
+            IDEConfig.INSTANCE.createFrame(file.getPath());
+            IDEConfig.INSTANCE.getFrame();
+
+            return true;
+        }
+        return false;
+    }
+
+}
