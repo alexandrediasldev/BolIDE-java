2ccaac10c1e41cbf975fd05cd844adbf6f80dc06
alexandre.dias <alexandre.dias@epita.fr>
2021-07-07 21:07:29 +0200

    Fixed maven projet run on windows, and launching jar working

--- src/main/java/fr/epita/assistants/myide/domain/entity/feature/maven/StreamHandler.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/feature/maven/StreamHandler.java
@@ -10,14 +10,17 @@ class StreamHandler extends Thread {
     private InputStream inputStream;
 
     public StreamHandler(InputStream inputStream) {
+        //System.out.println("Setup");
         this.inputStream = inputStream;
     }
 
     public void run(boolean isError) {
         try {
+            //System.out.println("Running");
             InputStreamReader reader = new InputStreamReader(inputStream);
             BufferedReader br = new BufferedReader(reader);
             String line = null;
+
             while ((line = br.readLine()) != null) {
                 if (isError)
                     System.err.println("Err:" + line);
