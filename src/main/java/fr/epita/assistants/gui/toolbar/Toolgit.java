c879416e3533c361e997aa4b9f3dcd948f9ef51b
Anonymous <anonymous@example.com>
2021-07-07 18:42:09 +0200

    added Toolgit.java

---
+++ src/main/java/fr/epita/assistants/gui/toolbar/Toolgit.java
@@ -0,0 +1,21 @@
+package fr.epita.assistants.gui.toolbar;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Toolgit extends JMenu {
+    public Toolgit() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("resources/macadamia_nut.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        var dimg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        setIcon(new ImageIcon(dimg));
+    }
+}
