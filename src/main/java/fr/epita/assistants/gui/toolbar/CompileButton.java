bb2a07ca9ad18d9845224730afaa8b3257e40f66
alexandre.dias <alexandre.dias@epita.fr>
2021-07-07 23:35:47 +0200

    Added middlebutons in a separate class

--- src/main/java/fr/epita/assistants/gui/toolbar/CompileButton.java
+++ src/main/java/fr/epita/assistants/gui/toolbar/CompileButton.java
@@ -2,6 +2,7 @@ package fr.epita.assistants.gui.toolbar;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +12,7 @@ public class CompileButton extends JButton implements ActionListener {
     public CompileButton()
     {
         Border emptyBorder = BorderFactory.createEmptyBorder();
+        setMaximumSize(new Dimension(35,35));
         setBorder(emptyBorder);
         setIcon(createIcon("src/main/resources/cog.png", 30, 30));
     }
