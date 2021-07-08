51ac0f7e7ccf4160215f1d56dfe6b76698598b75
alexandre.dias <alexandre.dias@epita.fr>
2021-07-08 15:14:30 +0200

    Added jediterm

---
+++ src/main/java/fr/epita/assistants/gui/shell/BasicTerm.java
@@ -0,0 +1,85 @@
+package fr.epita.assistants.gui.shell;
+
+
+
+import com.google.common.base.Ascii;
+import com.jediterm.pty.PtyProcessTtyConnector;
+import com.jediterm.terminal.Questioner;
+import com.jediterm.terminal.TtyConnector;
+import com.jediterm.terminal.ui.JediTermWidget;
+import com.jediterm.terminal.ui.TerminalWidget;
+import com.jediterm.terminal.ui.TerminalWidgetListener;
+import com.jediterm.terminal.ui.UIUtil;
+import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
+import com.jediterm.terminal.ui.settings.SettingsProvider;
+import com.pty4j.PtyProcess;
+import com.pty4j.PtyProcessBuilder;
+import fr.epita.assistants.gui.IDEConfig;
+import fr.epita.assistants.gui.IDEFrame;
+import fr.epita.assistants.myide.domain.entity.Project;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicTerm extends JPanel{
+
+    Project project;
+    public BasicTerm(Project project)
+    {
+        setLayout(new GridLayout());
+          this.project = project;
+          add(createTerminalWidget());
+
+    }
+    public JediTermWidget createTerminalWidget() {
+
+        SettingsProvider settings;
+        settings = new DefaultSettingsProvider();
+
+        JediTermWidget widget = new JediTermWidget(80, 10, settings);
+        widget.setTtyConnector(createTtyConnector());
+        widget.start();
+
+        return widget;
+    }
+
+    private TtyConnector createTtyConnector() {
+        try {
+            Map<String, String> envs = System.getenv();
+            String[] command;
+            if (UIUtil.isWindows) {
+                command = new String[]{"cmd.exe", "/K", "cd", String.valueOf(project.getRootNode().getPath())};
+            } else {
+                command = new String[]{"/bin/bash", "--login"};
+                envs = new HashMap<>(System.getenv());
+                envs.put("PWD",String.valueOf(project.getRootNode().getPath()));
+                envs.put("TERM", "xterm-256color");
+            }
+
+            PtyProcess process = new PtyProcessBuilder().setCommand(command).setEnvironment(envs).start();
+            //OutputStream is = process.getOutputStream();
+
+            //String comman = new String("cd "+ project.getRootNode().getPath()+"\r\n");
+            //is.write(comman.getBytes(StandardCharsets.UTF_8));
+
+
+
+            var connector = new PtyProcessTtyConnector(process, StandardCharsets.UTF_8);
+
+            return connector;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // Create and show this application's GUI in the event-dispatching thread.
+
+    }
+}
