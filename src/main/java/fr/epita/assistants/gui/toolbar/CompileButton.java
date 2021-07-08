7be16413820ef48ff59cde8e13f3198cf77543e5
Martin Chevereau <martin.chevereau@epita.fr>
2021-07-09 00:28:38 +0200

    generalized redirection

--- src/main/java/fr/epita/assistants/gui/toolbar/CompileButton.java
+++ src/main/java/fr/epita/assistants/gui/toolbar/CompileButton.java
@@ -2,6 +2,7 @@ package fr.epita.assistants.gui.toolbar;
 
 import fr.epita.assistants.gui.ArgumentDialog;
 import fr.epita.assistants.gui.IDEConfig;
+import fr.epita.assistants.gui.utils.Redirection;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 
 
@@ -14,10 +15,8 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -26,42 +25,6 @@ import static fr.epita.assistants.gui.utils.CreateIcon.createIcon;
 
 public class CompileButton extends JButton implements ActionListener {
     // all this field are here to redirect sout/serr into popup
-    ByteArrayOutputStream outStream;
-    ByteArrayOutputStream errStream;
-    StringBuilder outLog = new StringBuilder();
-    StringBuilder errLog = new StringBuilder();
-    PrintStream newOut = null;
-    PrintStream oldOut = null;
-    PrintStream newErr = null;
-    PrintStream oldErr = null;
-
-    public void StartRedirectError()
-    {
-        outStream = new ByteArrayOutputStream();
-        newOut = new PrintStream(outStream);
-        oldOut = System.out;
-        System.setOut(newOut);
-
-
-        errStream = new ByteArrayOutputStream();
-        newErr = new PrintStream(errStream);
-        oldErr = System.err;
-        System.setOut(newErr);
-    }
-
-    public void StopRedirectError()
-    {
-        outLog.setLength(0);
-        errLog.setLength(0);
-
-        System.out.flush();
-        System.setOut(oldOut);
-        outLog.append(outStream.toString());
-
-        System.err.flush();
-        System.setOut(oldErr);
-        errLog.append(errStream.toString());
-    }
 
     public CompileButton()
     {
@@ -80,7 +43,7 @@ public class CompileButton extends JButton implements ActionListener {
                 var proj = IDEConfig.INSTANCE.getFrame().getCurrentProject();
 
 
-                StartRedirectError();
+                Redirection.StartRedirectError();
 
 
                 var executeReport = IDEConfig.INSTANCE.getFrame().getP().execute(proj, Mandatory.Features.Maven.COMPILE, args);
@@ -108,11 +71,10 @@ public class CompileButton extends JButton implements ActionListener {
 
                     p.start();
 
-                    StopRedirectError();
+                    Redirection.StopRedirectError(this);
 
                     var compilationPopup = new CompilationPopup(true, "Compilation success");
                     compilationPopup.setVisible(true);
-                    JOptionPane.showMessageDialog(this, outLog.toString() + "\n" + errLog.toString());
                 }
                 else
                 {
@@ -139,8 +101,7 @@ public class CompileButton extends JButton implements ActionListener {
                     compilationPopup.setVisible(true);
 
 
-                    StopRedirectError();
-                    JOptionPane.showMessageDialog(this, outLog.toString() + "\n" + errLog.toString());
+                    Redirection.StopRedirectError(this);
                 }
             }
         }
